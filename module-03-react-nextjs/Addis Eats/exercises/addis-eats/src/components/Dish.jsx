d165db1d44faf0fc6318abed5b2416210d791746
kalkidanWoldesenbet <kalkidanwoldsenbet0121@gmail.com>
2026-08-28 11:16:47 +0300

    feat: add Card wrapper

--- module-03-react-nextjs/Addis Eats/exercises/addis-eats/src/components/Dish.jsx
+++ module-03-react-nextjs/Addis Eats/exercises/addis-eats/src/components/Dish.jsx
@@ -1,11 +1,14 @@
 import PropTypes from 'prop-types'
+import Card from './Card';
 
 function Dish({name, price,spicy, currency="ETB"}) {
   return (
     <div className='dish'>
-      <h2>{name}</h2>
-      <p>{price} {currency}</p>
-      <p><em>{spicy && <em>Spicy</em> }</em></p>
+      <Card>
+        <h2>{name}</h2>
+        <p>{price} {currency}</p>
+        <p><em>{spicy && <em>Spicy</em> }</em></p>
+      </Card>
     </div>
   )
 }
