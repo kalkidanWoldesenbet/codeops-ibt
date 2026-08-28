adf8a777a3f2f03f4e8080527ba381650f3e32f0
kalkidanWoldesenbet <kalkidanwoldsenbet0121@gmail.com>
2026-08-29 00:35:47 +0300

    feat: filter menu and render dishes by category

--- module-03-react-nextjs/Addis Eats/exercises/addis-eats/src/components/Main.jsx
+++ module-03-react-nextjs/Addis Eats/exercises/addis-eats/src/components/Main.jsx
@@ -3,17 +3,50 @@ import Dish from './Dish'
 
 function Main() {
     const menu = [
-        {name:"Doro wot", price: "400 ",spicy:true},
-        {name:"Kitfo", price: "500 ",spicy:false},
-        {name:"Pasta", price: "250 ",spicy:false},
+        {id:1,name:"Doro wot", price: "400 ",spicy:true,category:"Side Dish"},
+        {id:2,name:"Kitfo", price: "500 ",spicy:false, category:"Main Course"},
+        {id:3,name:"Pasta", price: "250 ",spicy:false, category:"Main Course"},
     ]
 
+    const mainCat = menu.filter(
+      (item)=> item.category === "Main Course"
+    )
+
+    const sideCat = menu.filter(
+      (item)=> item.category === "Side Dish"
+    )
+
+    if(mainCat.length === 0 && sideCat.length === 0){
+      return <p>No dishes found.</p>
+    }
   return (
-    <div className='card-container'>
-      {
-      menu.map((item, index) => (
-        <Dish key={index} name={item.name} price={item.price} spicy={item.spicy}/>
-      ))}
+    <div>
+      <h2>Main Courses</h2>
+     <div className='card-container'>
+       
+       {
+       mainCat.map((item) => (
+         <Dish 
+           key={item.id} 
+           name={item.name} 
+           price={item.price} 
+           spicy={item.spicy}
+         />
+       ))}
+     </div>
+       <h2>Side Dishes</h2>
+     <div className='card-container'>
+       {
+         sideCat.map((item) =>(
+           <Dish
+             key={item.id} 
+             name={item.name} 
+             price={item.price} 
+             spicy={item.spicy} 
+           />
+         ))
+       }
+      </div>
     </div>
   )
 }
