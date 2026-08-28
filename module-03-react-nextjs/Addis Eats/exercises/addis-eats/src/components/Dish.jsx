ec1f07d9ebab5e6aa3b40582574fa66a78c829ef
kalkidanWoldesenbet <kalkidanwoldsenbet0121@gmail.com>
2026-08-28 10:45:25 +0300

    feat: render spicy badge conditionally with &&

--- module-03-react-nextjs/Addis Eats/exercises/addis-eats/src/components/Dish.jsx
+++ module-03-react-nextjs/Addis Eats/exercises/addis-eats/src/components/Dish.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import PropTypes from 'prop-types'
 
-function Dish({name, price}) {
+function Dish({name, price,spicy, currency="ETB"}) {
   return (
     <div className='dish'>
       <h2>{name}</h2>
-      <p>{price}</p>
+      <p>{price} {currency}</p>
+      <p><em>{spicy && <em>Spicy</em> }</em></p>
     </div>
   )
 }
+
+Dish.propTypes = {
+  name:PropTypes.string.isRequired,
+  price:PropTypes.number.isRequired,
+  spicy:PropTypes.bool,
+  currency:PropTypes.string,
+};
 
 export default Dish
--- module-03-react-nextjs/Addis Eats/exercises/addis-eats/src/components/Main.jsx
+++ module-03-react-nextjs/Addis Eats/exercises/addis-eats/src/components/Main.jsx
@@ -3,16 +3,16 @@ import Dish from './Dish'
 
 function Main() {
     const menu = [
-        {name:"Doro wot", price: "400 ETB"},
-        {name:"Kitfo", price: "500 ETB"},
-        {name:"Pasta", price: "250 ETB"},
+        {name:"Doro wot", price: "400 ",spicy:true},
+        {name:"Kitfo", price: "500 ",spicy:false},
+        {name:"Pasta", price: "250 ",spicy:false},
     ]
 
   return (
     <div className='card-container'>
       {
       menu.map((item, index) => (
-        <Dish key={index} name={item.name} price={item.price}/>
+        <Dish key={index} name={item.name} price={item.price} spicy={item.spicy}/>
       ))}
     </div>
   )
