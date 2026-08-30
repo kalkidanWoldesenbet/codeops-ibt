650d3af9e520872fd9b2deefdd199170e29b1de3
kalkidanWoldesenbet <kalkidanwoldsenbet0121@gmail.com>
2026-08-30 19:46:35 +0300

    feat: complete Day 2 mini-project

--- module-03-react-nextjs/Addis Eats/miniprojs/addis-eats/src/components/Dish.jsx
+++ module-03-react-nextjs/Addis Eats/miniprojs/addis-eats/src/components/Dish.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import Card from './Card'
 
-function Dish({name, price}) {
+function Dish({name, price, currency="ETB", spicy, category}) {
   return (
     <div className='dish'>
-      <h3>{name}</h3>
-      <p>{price}</p>
+      <Card>
+        <h3>{name}</h3>
+        <p>{price} {currency}</p>
+        <p>{category}</p>
+        <p>{spicy && <em>Spicy</em> }</p>
+      </Card>
     </div>
   )
 }
+
+// Dish.PropTypes= {
+//   name:PropTypes.string.isRequired,
+//   price:PropTypes.number.isRequired,
+//   spicy:PropTypes.bool,
+//   currency:PropTypes.string,
+// };
 
 export default Dish
--- module-03-react-nextjs/Addis Eats/miniprojs/addis-eats/src/components/Main.jsx
+++ module-03-react-nextjs/Addis Eats/miniprojs/addis-eats/src/components/Main.jsx
@@ -1,20 +1,60 @@
 import React from 'react'
+import menu from "../data"
 import Dish from './Dish'
 
-const menu = [
-     {name:"Doro wot", price: "400 ETB"},
-     {name:"Kitfo", price: "500 ETB"},
-     {name:"Pasta", price: "250 ETB"},
- ]
+
 function Main() {
+  const mainCat = menu.filter(
+    (item) => item.category ==="Main Course"
+  );
+  const sideCat = menu.filter(
+    (item) => item.category ==="Side Dish"
+  );
+  const beverage = menu.filter(
+    (item) => item.category ==="Beverage"
+  );
+
+  if (
+      mainCat.length === 0 &&
+      sideCat.length === 0 &&
+      beverage.length === 0
+    ) {
+      return <p>No dishes found.</p>;
+    }
   return (
-    <div className='card-container'>
+    <div>
+      <h2>Main Courses</h2>
+      <div className='card-container'>
       {
-        menu.map((item,index) => (
-           <Dish key={index} name={item.name} price={item.price} /> 
+        mainCat.map((item) => (
+           <Dish key={item.id} 
+            {...item}
+            /> 
         ))
       }
+      </div>
+      <h2>Side Dishes</h2>
+      <div className='card-container'>
+        {
+          sideCat.map((item) => (
+             <Dish key={item.id} 
+              {...item}
+              /> 
+          ))
+        }
+      </div>
+      <h2>Beverages</h2>
+      <div className='card-container'>
+        {
+          beverage.map((item) => (
+             <Dish key={item.id} 
+              {...item}
+              /> 
+          ))
+        }
+      </div>
     </div>
+    
   )
 }
 
