6c1d70779df3cf270c53f6e09328ae57be78605b
kalkidanWoldesenbet <kalkidanwoldsenbet0121@gmail.com>
2026-08-30 21:03:43 +0300

    feat: add dish count state

--- module-03-react-nextjs/Addis Eats/exercises/addis-eats/src/components/Dish.jsx
+++ module-03-react-nextjs/Addis Eats/exercises/addis-eats/src/components/Dish.jsx
@@ -1,23 +1,24 @@
-import PropTypes from 'prop-types'
+import { useState } from 'react';
 import Card from './Card';
 
+
 function Dish({name, price,spicy, currency="ETB"}) {
+  
+  const [count,setCount] = useState(0);
+
   return (
     <div className='dish'>
       <Card>
-        <h2>{name}</h2>
+        <h2>{name} Quantity: {count}</h2>
         <p>{price} {currency}</p>
-        <p><em>{spicy && <em>Spicy</em> }</em></p>
+        <p>{spicy && <em>Spicy</em> }</p>
+        <button onClick={()=>{setCount(count + 1)}}>Add</button>
+        
       </Card>
     </div>
   )
 }
 
-Dish.propTypes = {
-  name:PropTypes.string.isRequired,
-  price:PropTypes.number.isRequired,
-  spicy:PropTypes.bool,
-  currency:PropTypes.string,
-};
+
 
 export default Dish
